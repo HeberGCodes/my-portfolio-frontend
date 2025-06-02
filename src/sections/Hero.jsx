05f2bd164927583d592d1b21a8b6e7175b683d4f
Heber Garcia Castillo <hebergcodes@gmail.com>
2025-06-02 12:07:23 +0200

    Hero: update intro text

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,13 @@
 export default function Hero() {
     return (
-      <div className="min-h-screen flex flex-col justify-center items-center bg-zinc-800">
-        <h1 className="text-5xl text-white font-bold mb-4 text-center">Hi, I’m Heber</h1>
-        <p className="text-xl text-zinc-100 text-center max-w-lg">
-          I'm an aspiring software and web developer building clean and functional digital experiences.
+      <div className="min-h-screen flex flex-col justify-center items-center bg-zinc-800 text-white p-6">
+        
+        <h1 className="text-5xl sm:text-6xl font-bold mb-4 text-center">
+          Hi, I’m Heber
+        </h1>
+        <p className="text-xl sm:text-xl text-zinc-100 max-w-lg text-center mb-6">
+          Welcome to my portfolio website! 
+          <br/> Check out my projects and learn more about me.
         </p>
       </div>
     );
