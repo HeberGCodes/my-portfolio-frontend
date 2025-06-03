220cac3b6c55478d1ab5b379cd127d316ebc2a10
Heber Garcia Castillo <hebergcodes@gmail.com>
2025-06-03 02:24:51 +0200

    Hero: adjust spacing for improved layout

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,12 +2,13 @@ export default function Hero() {
     return (
       <div className="min-h-screen flex flex-col justify-center items-center bg-zinc-800 text-white p-6">
         
-        <h1 className="text-5xl sm:text-6xl font-bold mb-4 text-center">
+        <h1 className="text-5xl sm:text-6xl font-bold mb-8 text-center">
           Hi, I’m Heber
         </h1>
-        <p className="text-xl sm:text-xl text-zinc-100 max-w-lg text-center mb-6">
+        <p className="text-xl sm:text-xl text-zinc-100 max-w-xl text-center mb-6">
           Welcome to my portfolio website! 
-          <br/> Check out my projects and learn more about me.
+          <br/>
+          Check out my projects and learn more about me.
         </p>
       </div>
     );
