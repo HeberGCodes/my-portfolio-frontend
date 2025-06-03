2fa720399cb83e75cd93b67430e265ccbb2a4b31
Heber Garcia Castillo <hebergcodes@gmail.com>
2025-06-03 02:23:54 +0200

    About: adjust spacing for improved layout

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
     return (
       <div className="min-h-screen flex flex-col items-center bg-amber-300 text-zinc-900 p-6 pt-24 pb-12">
         
-        <h2 className="text-6xl font-bold text-center mb-12">About Me</h2>
+        <h2 className="text-6xl font-bold text-center mb-8">About Me</h2>
         <p className="text-xl text-zinc-900 text-center max-w-xl">
           I’m passionate about web and software development, especially Python and Django. I enjoy solving problems, building real-world applications, and constantly learning.
           This site showcases my work, goals, and what I bring to the table.
