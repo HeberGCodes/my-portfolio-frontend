e2173658e13e39406218014b2e313b76e13f1546
Heber Garcia Castillo <hebergcodes@gmail.com>
2025-06-05 12:38:40 +0200

    Footer: remove 'All rights reserved' text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
       <footer className="bg-gray-900 text-gray-400 text-center py-6">
-        <p>&copy; {new Date().getFullYear()} HeberGCodes. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} HeberGCodes</p>
       </footer>
     );
   }
