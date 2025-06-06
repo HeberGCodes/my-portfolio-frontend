8a52ed5330d9b2284ce1f88712e96ea9d4412638
Heber Garcia Castillo <hebergcodes@gmail.com>
2025-06-06 11:32:20 +0200

    Footer: make fixed at bottom and add social links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,35 @@
+import { FaLinkedin, FaGithub, FaXTwitter } from "react-icons/fa6";
+
 export default function Footer() {
     return (
-      <footer className="bg-gray-900 text-gray-400 text-center py-6">
+      <footer className="bg-zinc-900 text-zinc-500 text-sm text-center p-1 fixed bottom-0 w-full">
+        {/* Social Links */}
+              <div className="justify-center flex gap-6 text-3xl py-2">
+                <a
+                    href="https://www.linkedin.com/in/heber-gc/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-teal-300 hover:text-amber-300 transition transform hover:scale-110"
+                >
+                    <FaLinkedin />
+                </a>
+                <a
+                  href="https://github.com/HeberGCodes"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-teal-300 hover:text-amber-300 transition transform hover:scale-110"
+                >
+                  <FaGithub />
+                </a>
+                <a
+                  href="https://x.com/HeberGCodes"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-teal-300 hover:text-amber-300 transition hover:scale-110"
+                >
+                  <FaXTwitter />
+                </a>
+              </div>
         <p>&copy; {new Date().getFullYear()} HeberGCodes</p>
       </footer>
     );
